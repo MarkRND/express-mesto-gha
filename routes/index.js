6cda958a3104b3db25cbf3dcc5394bd1256dc88e
Gorlov Mark <masmik@rambler.ru>
2023-06-05 04:12:21 +0300

    add 13.0.6

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
+const { messageError } = require("../messageError");
 const usersRoute = require("./users");
 const usersCards = require("./cards");
 
 router.use("/users", usersRoute);
 router.use("/cards", usersCards);
+router.use((req, res) => {
+  const err = new Error("Неверный адрес");
+  err.name = "NotFoundError";
+  messageError(err, req, res);
+});
 
 module.exports = router;
