5ccbb8b51bc7287708e4b3e7bc4c3a501a78e2dd
Gorlov Mark <masmik@rambler.ru>
2023-06-05 03:46:38 +0300

    add 13.0.3

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 const User = require("../models/user");
 const { messageError } = require("../messageError");
 
@@ -47,20 +48,27 @@ const updateAvatar = async (req, res) => {
     messageError(err, req, res);
   }
 };
-async function editUser(req, res) {
+const editUser = async (req, res) => {
   try {
     const userId = req.user._id;
     const { name, about } = req.body;
+    if (!name || !about) {
+      return res.status(400).json({ message: "Переданы некорректные данные" });
+    }
     const user = await User.findByIdAndUpdate(
       userId,
       { name, about },
       { new: true, runValidators: true },
     );
+
+    if (!user) {
+      return res.status(404).json({ message: "Пользователь не найден" });
+    }
     res.send(user);
   } catch (err) {
-    messageError(err, req, res);
+    res.status(500).json({ message: "Ошибка сервера" });
   }
-}
+};
 
 module.exports = {
   getInfoUsers,
--- messageError.js
+++ messageError.js
@@ -1,18 +1,11 @@
 /* eslint-disable no-constant-condition */
 const messageError = (err, req, res) => {
-  if ((err.name === "CastError") || (err.name === "ValidationError")) {
+  if (err.name === "CastError") {
     res.status(400).send({
       message: "переданы некорректные данные ",
     });
     return;
   }
-
-  // if (err.name === "ValidationError") {
-  //   res.status(400).send({
-  //     message: "Переданы некорректные данные",
-  //   });
-  //   return;
-  // }
 
   if (err.name === "NotFoundError") {
     res.status(404).send({
