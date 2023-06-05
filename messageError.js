700096eb3bf47e32f05acee92d709bd8717edb96
Gorlov Mark <masmik@rambler.ru>
2023-06-05 03:57:07 +0300

    add 13.0.4

--- messageError.js
+++ messageError.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-constant-condition */
 const messageError = (err, req, res) => {
-  if (err.name === "CastError") {
+  if ((err.name === "CastError") || (err.name === "ValidationError")) {
     res.status(400).send({
       message: "переданы некорректные данные ",
     });
     return;
   }
+
 
   if (err.name === "NotFoundError") {
     res.status(404).send({
