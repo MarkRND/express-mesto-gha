8a2e5f61fe051ef79dc92fdc1e8a0d0242682878
Gorlov Mark <masmik@rambler.ru>
2023-06-18 16:58:57 +0300

    helmet and limit-on

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit')
 const { errors } = require('celebrate');
 
 const router = require("./routes");
@@ -8,10 +10,18 @@ const GlobalError = require("./middlwares/GlobalError");
 const { PORT = 3000 } = process.env;
 const app = express();
 
+const appLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {
   useNewUrlParser: true,
 });
-
+app.use(appLimiter);
+app.use(helmet());
 app.use(express.json());
 app.use(router);
 app.use(errors());
