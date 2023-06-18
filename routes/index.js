ce7992bbbe9ea4090891d30a44ba031d6c7fcc4e
Gorlov Mark <masmik@rambler.ru>
2023-06-18 03:30:13 +0300

    add 14.0.14

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
-// const { messageError } = require("../messageError/messageError");
-const ForbiddenError = require("../messageError/Forbidden");
+
+const NotFoundError = require("../messageError/NotFoundError");
 const usersRoute = require("./users");
 const usersCards = require("./cards");
 const { addUser, login } = require("../controllers/users");
 const auth = require("../middlwares/auth");
-const { validationSignin, validationSignup } = require("../middlwares/celebrateJoi");
+const {
+  validationSignin,
+  validationSignup,
+} = require("../middlwares/celebrateJoi");
 
 router.post("/signup", validationSignup, addUser);
 router.post("/signin", validationSignin, login);
@@ -14,7 +17,7 @@ router.use(auth);
 router.use("/users", usersRoute);
 router.use("/cards", usersCards);
 router.use(() => {
-  throw new ForbiddenError('Указан неправильный путь');
+  throw new NotFoundError("Страница не найдена");
 });
 
 module.exports = router;
